84a4d4ed1b7edde13ed6ceaaf0987f956a50ff79
sddysz <sddysz@126.com>
2017-05-06 11:20:50 +0800

    调整login页

--- home/views.py
+++ home/views.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # adminPages.py
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template
+from flask_login import current_user
 
 
 home = Blueprint('home', __name__)
 
 
+# leanote展示页, 没有登录的, 或已登录明确要进该页的
 @home.route('/')
-def admin_login():
-    return render_template('home/index.html')
+def index():
 
-
-
+    return render_template('home/index.html', userInfo=current_user, title='dysz')
